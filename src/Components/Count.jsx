3a0333efaa7104a777c01372d5c8822507497459
अभय सिंह <99662885+abhaysingh28@users.noreply.github.com>
2023-02-28 23:30:55 +0530

    Update Count.jsx

--- src/Components/Count.jsx
+++ src/Components/Count.jsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 function Count(initialValue = 0) {
     const [Counter, setCounter] = useState(initialValue);
     function increment() {
-        setCounter(Counter + 1);
+        setCounter(Counter +1);
     }
     function decrement() {
-        setCounter(Counter - 1);
+        setCounter(Counter -1);
     }
     return [Counter, increment, decrement];
 }
